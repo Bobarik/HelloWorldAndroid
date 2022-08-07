7f1c3fa57cba5e3bab3253eca80934be40e2e886
Bobarik <www.skorobogatov@mail.ru>
2022-08-07 15:23:31 +0300

    Second commit, app can count how many times the button was pressed

--- app/src/main/java/com/gmail/vlaskorobogatov/helloworldandroid/MainActivity.kt
+++ app/src/main/java/com/gmail/vlaskorobogatov/helloworldandroid/MainActivity.kt
@@ -2,10 +2,31 @@ package com.gmail.vlaskorobogatov.helloworldandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+private val TAG = MainActivity::class.qualifiedName
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate started")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate finished")
+    }
+
+    fun showToast(view: View) {
+        val toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun countUp(view: View) {
+        count++
+        show_count.text = count.toString()
     }
 }
